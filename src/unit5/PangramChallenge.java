2b7b569cd0c3d41b40243b4ffaf895c7a564c3fe
professorwade <121523833+professorwade@users.noreply.github.com>
2024-02-14 16:20:36 -0500

    Unit 5 String/Loop challenges

---
+++ src/unit5/PangramChallenge.java
@@ -0,0 +1,30 @@
+package unit5;
+
+/*
+A pangram is a sentence containing every letter in the English Alphabet.
+Write a function to test if the passed string in a pangram.
+
+Input: “The quick brown fox jumps over the lazy dog”
+Output: is a Pangram
+Explanation: Contains all the characters from ‘a’ to ‘z’]
+
+ */
+public class PangramChallenge {
+
+    public static boolean isAPangram(String s) {
+        if (s.length() < 26)
+            return false; // not enough letters
+
+
+
+
+
+        return false; // assume it is not.
+    }
+
+    public static void Main(String[] args) {
+
+    }
+
+
+}
---
+++ src/unit5/StringReverseChallenge.java
@@ -0,0 +1,14 @@
+package unit5;
+
+/*
+Given a string, the task is to write a function the returns a string in the reverse order.
+
+Example:
+reverseString(“i love programming very much”) should return:
+“much very programming love i”
+
+ Note, you may do the challenge "in place" or create a new returned string.
+ */
+
+public class StringReverseChallenge {
+}
