7c48114ee9c5d757dd30ebf8f7403bd669df848b
professorwade <schofieldw@centralvirginia.edu>
2024-03-07 08:23:06 -0500

    Unit 9 - Practice Challenge

---
+++ src/unit9/CalculatorClassAssignment.java
@@ -0,0 +1,29 @@
+package unit9;
+
+/**
+ * Instructions:
+ * Create a class that supports the basic four functions of a calculator.
+ * Methods:
+ * add(double a, double b), add()
+ * multiply(double a, double b), multiply()
+ * divide(double a, double b), divide() -- check for zero in divisor
+ * subtract(double a, double b), subtract()
+ *
+ * Getters:
+ * double[] getArguments() -- return arguments in a two elements array arg[0] - a, arg[1] - b
+ *
+ * Setters:
+ * void setArguments(double a, double b)
+ *
+ *
+ * Your class should retain the last two numbers passed for subsequent no
+ * argument method calls.
+ *
+ * Constructors:
+ * Calculator()
+ * Calculator(double a, double b)
+ *
+ * Write a main method that exercises all the class's functionality.
+ */
+public class CalculatorClassAssignment {
+}
